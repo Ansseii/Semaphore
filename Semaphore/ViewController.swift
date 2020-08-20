d54886d3c5bfa6c8722969fea726e5e050e7c1fc
Aleksandr Lukash <a.lukash.92@gmail.com>
2020-08-20 10:23:27 +0300

    Task 2 has been done

--- Semaphore/ViewController.swift
+++ Semaphore/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var signalsStackView: UIStackView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    private let activate: (UIView) -> Void = { $0.alpha = 1 }
+    private let deactivate: (UIView) -> Void = { $0.alpha = 0.3 }
+    
+    private var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        signalsStackView.arrangedSubviews.forEach { signal in
+            signal.frame = CGRect(x: 0,y: 0,width: 100,height: 100)
+            signal.layer.cornerRadius = signal.frame.size.width / 2
+            signal.layer.masksToBounds = true
+            deactivate(signal)
+        }
     }
-
-
+    
+    @IBAction func action(_ sender: Any) {
+        actionButton.setTitle("Next", for: .normal)
+        
+        let signalsCount = signalsStackView.arrangedSubviews.count;
+        let views = signalsStackView.arrangedSubviews
+        
+        if currentIndex > signalsCount - 1 {
+            currentIndex = 0
+        }
+        
+        if currentIndex > 0 {
+            activate(views[currentIndex])
+            deactivate(views[currentIndex - 1])
+            currentIndex += 1
+        } else {
+            activate(views[currentIndex])
+            deactivate(views[signalsCount - 1])
+            currentIndex += 1
+        }
+    }
 }
 
